cdca98902b34f43d02e4599a6c4c9bda38413cf8
Taynara <taynarasantossousa09@gmail.com>
2025-04-08 13:37:18 -0300

    Add files via upload

---
+++ ex001/script.js
@@ -0,0 +1,5 @@
+var valor = Number(prompt("Digite o valor do produto:"))
+var desconto = Number(prompt(" Digite o valor do desconto:"))
+var valorFinal = valor - ( valor * desconto)
+
+alert("O valor do produto com desconto é: " + valorFinal)
---
+++ ex002/script.js
@@ -0,0 +1,7 @@
+var numero1 = Number(prompt("Digite um número:"))
+var numero2 = Number(prompt("Digite o segundo número:"))
+var numero3 = Number(prompt("Digite o terceiro número:"))
+
+if(numero1 > numero2 && numero2 > numero3) {
+    alert("O maior número é:")
+}
